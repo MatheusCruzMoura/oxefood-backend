ac47c3291aee38a7d8f3a545ac9b12b3da66679a
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2023-01-16 16:52:37 -0300

    buscar todas as lojas e loja por id

--- src/main/java/br/com/ifpe/oxefood/modelo/loja/LojaService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/loja/LojaService.java
@@ -1,10 +1,13 @@
 package br.com.ifpe.oxefood.modelo.loja;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood.modelo.fornecedor.Fornecedor;
 import br.com.ifpe.oxefood.util.entity.GenericService;
 
 @Service
@@ -26,5 +29,11 @@ public class LojaService extends GenericService {
 	public Loja obterLojaPorID(Long id) {
 		return repository.findById(id).get();
 	}
+	
+	@Transactional
+    public List<Loja> listarTodos() {
+
+    	return repository.findAll();
+    }
 
 }
--- src/main/java/br/com/ifpe/oxefood/servicos/loja/LojaController.java
+++ src/main/java/br/com/ifpe/oxefood/servicos/loja/LojaController.java
@@ -1,15 +1,20 @@
 package br.com.ifpe.oxefood.servicos.loja;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ifpe.oxefood.modelo.fornecedor.Fornecedor;
 import br.com.ifpe.oxefood.modelo.loja.Loja;
 import br.com.ifpe.oxefood.modelo.loja.LojaService;
 import br.com.ifpe.oxefood.util.entity.GenericController;
@@ -28,4 +33,17 @@ public class LojaController extends GenericController {
 		Loja lojaCriada = lojaService.save(loja);
 		return new ResponseEntity<>(lojaCriada, HttpStatus.CREATED);
 	}
+	
+	@GetMapping("/{id}")
+	public Loja obterLojaPorID(@PathVariable Long id) {
+
+		return lojaService.obterLojaPorID(id);
+	}
+	
+	@GetMapping
+    public List<Loja> listarTodos() {
+
+		return lojaService.listarTodos();
+    }
+	
 }
